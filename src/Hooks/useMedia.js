1b8c78e1c753096bd3114eadf0a4328e8c43f015
nlom0218 <nlom0218@naver.com>
2021-11-14 15:24:16 +0900

    useMedia hook

---
+++ src/Hooks/useMedia.js
@@ -0,0 +1,25 @@
+import { useMediaQuery } from 'react-responsive';
+
+const useMedia = () => {
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 1024px)"
+  })
+  const isTablet = useMediaQuery({
+    query: "(min-width: 768px)"
+  })
+  const isMobile = useMediaQuery({
+    query: "(min-width: 320px"
+  })
+  const media = () => {
+    if (isDesktop) {
+      return "Desktop"
+    } else if (isTablet) {
+      return "Tablet"
+    } else if (isMobile) {
+      return "Mobile"
+    }
+  }
+  return media
+}
+
+export default useMedia;
