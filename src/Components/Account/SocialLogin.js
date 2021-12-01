7ad18eafaf4b17ea79376254787c0856586fcb41
nlom0218 <nlom0218@naver.com>
2021-12-01 22:44:55 +0900

    naver login #2

--- src/Components/Account/SocialLogin.js
+++ src/Components/Account/SocialLogin.js
@@ -9,30 +9,6 @@ const SSocialLogin = styled.div`
   row-gap: 1.25rem;
 `
 
-const LoginLayout = styled.div`
-  background: ${props => props.bgColor};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 12px;
-  padding: 0.75rem;
-  border-radius: 12px;
-  border-radius: 0.75rem;
-  cursor: pointer;
-  svg {
-    margin-right: 10px;
-    margin-right: 0.625rem;
-    font-size: 24px;
-    font-size: 1.5rem;
-    color: ${props => props.iconColor};
-  }
-`
-
-const LoginText = styled.div`
-  color: ${props => props.textColor};
-  opacity: 0.85;
-`
-
 const NaverLogin = styled.div`
   width: 100%;
   text-align: center;
--- src/Pages/NaverLoginCallBack.js
+++ src/Pages/NaverLoginCallBack.js
@@ -1,8 +1,36 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router';
+import { useNavigate } from 'react-router';
+import { logInUser } from '../apollo';
+import routes from '../routes';
+
+const NAVER_LOGIN_MUTATION = gql`
+  mutation NaverLogin($email: String!) {
+    naverLogin(email: $email) {
+      ok
+      token
+    }
+  }
+`
 
 const NaverLoginCallBack = () => {
+  const navigate = useNavigate()
   const [email, setEmail] = useState(undefined)
+  const onCompleted = (result) => {
+    console.log(result)
+    const { naverLogin: { ok, error, token } } = result
+    if (error) {
+      // setErrMsg(error)
+    }
+    if (ok) {
+      logInUser(token)
+      navigate(routes.home)
+    }
+  }
+  const [naverLoginMutation, { loading }] = useMutation(NAVER_LOGIN_MUTATION, {
+    onCompleted
+  })
   const inItNaverLogin = () => {
     const naverLogin = new window.naver.LoginWithNaverId({
       clientId: "Gf04qU_FzIfDWz4a9_6Z",
@@ -19,15 +47,21 @@ const NaverLoginCallBack = () => {
       }
     })
     if (email) {
-
+      if (loading) {
+        return
+      }
+      naverLoginMutation({
+        variables: {
+          email
+        }
+      })
     }
   }
   useEffect(() => {
     inItNaverLogin()
-  })
+  }, [email])
   return <div>
     <div id="naverIdLogin"></div>
-
   </div>
 }
 
