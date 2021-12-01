e6a2e08bca2f9092c7af7f7d63c4250984224b36
nlom0218 <nlom0218@naver.com>
2021-12-01 19:20:12 +0900

    --

--- src/Pages/TodoList.js
+++ src/Pages/TodoList.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import BasicContainer from '../Components/Shared/BasicContainer';
+import useMe from "../Hooks/useMe"
 
 
 const TodoList = () => {
+  const { email } = useMe()
+  console.log(email);
   return (<BasicContainer>
     할일
   </BasicContainer>);
