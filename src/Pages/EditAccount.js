1faab6b40733299c16b1f160ca07c96a456b4932
nlom0218 <nlom0218@naver.com>
2021-12-03 00:06:16 +0900

    EditAccount findSchool #1

--- src/Pages/EditAccount.js
+++ src/Pages/EditAccount.js
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react';
 import BasicContainer from '../Components/Shared/BasicContainer';
 
 const EditAccount = () => {
-  useEffect(() => {
-    fetch("https://open.neis.go.kr/hub/schoolInfo?KEY=8bd04fadaf4d480792216f84d92fb1f9&Type=json&pIndex=1&pSize=10&SCHUL_NM=다목초")
+  const findSchool = (name) => {
+    fetch(`https://open.neis.go.kr/hub/schoolInfo?KEY=8bd04fadaf4d480792216f84d92fb1f9&Type=json&pIndex=1&pSize=10&SCHUL_NM=${name}`)
       .then(res => res.json())
-      .then(data => console.log(data))
-  }, [])
+      .then(data => console.log(data.schoolInfo[1].row))
+  }
   return (<BasicContainer>
-    회원정보 변경
+    <div onClick={() => findSchool("원당초")}>다목초등학교</div>
   </BasicContainer>);
 }
 
