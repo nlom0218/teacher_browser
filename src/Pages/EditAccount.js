c316ebebc6fc40b429d7479a97ce8d6f96c377c7
nlom0218 <nlom0218@naver.com>
2021-12-02 23:58:58 +0900

    school info fetch

--- src/Pages/EditAccount.js
+++ src/Pages/EditAccount.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BasicContainer from '../Components/Shared/BasicContainer';
 
 const EditAccount = () => {
+  useEffect(() => {
+    fetch("https://open.neis.go.kr/hub/schoolInfo?KEY=8bd04fadaf4d480792216f84d92fb1f9&Type=json&pIndex=1&pSize=10&SCHUL_NM=다목초")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
   return (<BasicContainer>
     회원정보 변경
   </BasicContainer>);
