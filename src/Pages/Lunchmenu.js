6c5a3db0730223554e799cfd2d34c5aa11c848eb
teacher-kiwi <kwi07@hanmail.net>
2021-12-05 16:06:58 +0900

    학교 검색하여 급식 불러오기 기능 추가(백앤드 연결)

--- src/Pages/Lunchmenu.js
+++ src/Pages/Lunchmenu.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import BasicContainer from "../Components/Shared/BasicContainer";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { gql, useQuery } from "@apollo/client";
 import { ko } from "date-fns/esm/locale";
+
 import dotenv from "dotenv";
-dotenv.config({ path: `${__dirname}/../../.env` });
+dotenv.config();
 
 const Title = styled.h1`
   font-size: 3em;
@@ -13,31 +16,58 @@ const Title = styled.h1`
   margin: 1em;
 `;
 
+const GET_SCHOOL = gql`
+  query SearchSchool($name: String!) {
+    searchSchool(name: $name) {
+      SCHUL_NM
+      ATPT_OFCDC_SC_CODE
+      SD_SCHUL_CODE
+    }
+  }
+`;
+
 const Lunchmenu = () => {
   const [date, setDate] = useState(new window.Date());
+  const [schoolName, setSchoolName] = useState("");
+  const [officeCode, setOfficeCode] = useState("");
+  const [schoolCode, setSchoolCode] = useState("");
   const [menu, setMenu] = useState("");
 
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => setSchoolName(data);
+
+  const School = (variables) => {
+    const { loading, data } = useQuery(GET_SCHOOL, variables);
+    if (loading) {
+      return "로딩중";
+    }
+    if (data) {
+      setOfficeCode(data.searchSchool[0].ATPT_OFCDC_SC_CODE);
+      setSchoolCode(data.searchSchool[0].SD_SCHUL_CODE);
+    }
+    return "";
+  };
+
   function getMenu() {
-    const OFFICE_CODE = "J10";
-    const SCHOOL_CODE = "7741022";
     const changedDate = `${date.getFullYear()}${(date.getMonth() + 1)
       .toString()
       .padStart(2, 0)}${date.getDate().toString().padStart(2, 0)}`;
-    console.log(process.env.REACT_APP_MENU_API_KEY);
     fetch(
       `https://open.neis.go.kr/hub/mealServiceDietInfo` +
         `?KEY=${process.env.REACT_APP_MENU_API_KEY}` +
         `&Type=json` +
         `&pIndex=1` +
         `&pSize=100` +
-        `&ATPT_OFCDC_SC_CODE=${OFFICE_CODE}` +
-        `&SD_SCHUL_CODE=${SCHOOL_CODE}` +
+        `&ATPT_OFCDC_SC_CODE=${officeCode}` +
+        `&SD_SCHUL_CODE=${schoolCode}` +
         `&MLSV_YMD=${changedDate}`
     )
       .then((response) => response.json())
       .then((json) => {
         json.RESULT
-          ? setMenu("해당 일에 급식 정보가 없습니다.")
+          ? json.RESULT.CODE === "ERROR-300"
+            ? setMenu("학교 이름을 입력해주세요.")
+            : setMenu("해당 일에 급식 정보가 없습니다.")
           : setMenu(json.mealServiceDietInfo[1].row[0].DDISH_NM);
       });
   }
@@ -55,7 +85,16 @@ const Lunchmenu = () => {
         locale={ko}
         withPortal
       />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="학교 이름을 입력해주세요."
+          {...register("name", { required: true })}
+        />
+        <button>확인</button>
+      </form>
       {menu}
+      <br />
+      <School variables={schoolName} />
     </BasicContainer>
   );
 };
