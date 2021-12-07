f51470f9d4b444128e1e9fb282c25c6d48b1dcdc
SHJcompany <sksthsaudgml@gmail.com>
2021-12-07 22:14:17 +0900

    123

--- src/Pages/Draw.js
+++ src/Pages/Draw.js
@@ -7,4 +7,27 @@ const Draw = () => {
   </BasicContainer>);
 }
 
+
 export default Draw;
+
+
+let lotto = [];
+
+for(let i=0; i<6; i++){
+    let num = Math.floor(Math.random() * 44) + 1;
+
+    for(let j in lotto){
+        if(num == lotto[j]){
+            num = Math.floor(Math.random() * 44) +1;
+        }
+    }
+    lotto.push(num);
+}
+
+lotto.sort(function(a,b){
+    return a - b;
+});
+
+document.write('번호 뽑기 결과 : ' + lotto);
+
+
