5307098bbb885e81cf71c170cdf449d4e0d7263c
SHJcompany <sksthsaudgml@gmail.com>
2021-12-16 06:52:08 +0900

    테스트1

--- src/Pages/TodoList.js
+++ src/Pages/TodoList.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import BasicContainer from '../Components/Shared/BasicContainer';
 import useMe from "../Hooks/useMe"
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from '../Components/Test/TodoTemplate';
+import TodoHead from '../Components/Test/TodoHead';
+import TodoList from '../Components/Test/TodoList';
+import TodoCreate from '../Components/Test/TodoCreate';
+
+// const TodoList = () => {
+//   const me = useMe()
+//   console.log(me?.email);
+//   return (<BasicContainer>
+//     할일
+//   </BasicContainer>);
+// }
+
+// export default TodoList;
 
 
-const TodoList = () => {
-  const me = useMe()
-  console.log(me?.email);
-  return (<BasicContainer>
-    할일
-  </BasicContainer>);
+const GlobalStyle = createGlobalStyle`
+
+`;
+
+function Todo() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </>
+  );
 }
 
-export default TodoList;
+export default Todo;
