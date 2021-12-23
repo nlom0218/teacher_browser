51df614b0f619b94c25220678bf029b3f4d4d5f9
anomist3 <superanomie89@gmail.com>
2021-12-23 21:18:24 +0900

    Countdown Error

--- src/Pages/Timer.js
+++ src/Pages/Timer.js
@@ -1,49 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import BasicContainer from '../Components/Shared/BasicContainer';
+import { CountUpDisplay, CountDownDisplay, BtnStartCountUp, BtnStartCountDown } from '../Components/Timer/TimeSet';
+import styled from 'styled-components';
+
+const ClockBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+`;
+
+const ClockContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 600px;
+  height: 600px;
+  background-color: white;
+  border-radius: 300px;
+`;
+
+const CountUp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+  width: 500px;
+  height: 500px;
+  border-radius: 500px;
+  background-color: black;
+  padding: 50px;
+  color: lime;
+  font-size: 4.5rem;
+`;
+
+const CountDownTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 50px;
+  background-color: 'white';
+  border: 1px solid red;
+`
 
 const Timer = () => {
-  const DisplayComponent = (props) => {
-    return (
-      <div>
-        <span>{(props.time.h >= 10) ? props.time.h : "0" + props.time.h}</span>&nbsp;:&nbsp;
-        <span>{(props.time.m >= 10) ? props.time.m : "0" + props.time.m}</span>&nbsp;:&nbsp;
-        <span>{(props.time.s >= 10) ? props.time.s : "0" + props.time.s}</span>&nbsp;:&nbsp;
-        <span>{(props.time.ms >= 10) ? props.time.ms : "0" + props.time.ms}</span>
-      </div>
-    )
-  };
 
-  // COUNT UP
+  const [time, setTime] = useState({ s: 0, m: 0, h: 0 });
 
-  const BtnStartCountUpComponent = (props) => {
-    return (
-      <div>
-        <button onClick={props.start} className='stopwatch-btn'>Count Up</button>
-      </div>
-    )
-  };
-
-  const BtnStartCountDownComponent = (props) => {
-    return (
-      <div>
-        <button onClick={props.start}>{props.text}</button>
-      </div>
-    )
-  }
-
-  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
+  const [timeLeft, setTimeLeft] = useState({ s: 10, m: 0, h: 0 });
 
   const startCountUp = () => {
     countUp();
-    setInterval(countUp, 10);
+    setInterval(countUp, 1000);
   }
 
   const startCountDown = () => {
     countDown();
-    setInterval(countDown, 10);
+    setInterval(countDown, 1000);
   }
 
-  let updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+  let updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  let updatedSLeft = timeLeft.s, updatedMLeft = timeLeft.m, updatedHLeft = timeLeft.h;
 
   const countUp = () => {
     if (updatedM === 60) {
@@ -54,54 +76,44 @@ const Timer = () => {
       updatedM++;
       updatedS = 0;
     };
-    if (updatedMs === 100) {
-      updatedS++;
-      updatedMs = 0;
-    };
-    updatedMs++;
-    return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
+    updatedS++;
+    return setTime({ s: updatedS, m: updatedM, h: updatedH });
   };
-
-  // COUNT DOWN
-
-  let [timeLeft, setTimeLeft] = useState({ ms: 0, s: 10, m: 0, h: 0 });
 
   const countDown = () => {
 
-    console.log('COUNT DOWN');
-    if (updatedM === 0 && updatedS === 0 && updatedMs === 0) {
-      updatedH--;
-      updatedM = 60;
+    if (updatedSLeft === 0) {
+      updatedMLeft--;
+      updatedSLeft += 60;
     };
-    if (updatedS === 0 && updatedMs === 0) {
-      updatedM--;
-      updatedS = 60;
+    if (updatedMLeft === 0 && updatedSLeft === 0) {
+      updatedHLeft--;
     };
-    if (updatedMs === 0) {
-      updatedS--;
-      updatedMs = 100;
-    };
-    updatedMs++;
-    return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
+
+    return setTimeLeft({ s: updatedSLeft, m: updatedMLeft, h: updatedHLeft });
   };
 
   return (<BasicContainer menuItem={true}>
-    <div className='main-section'>
-      <div className='clock-holder'>
-        <div className='stopwatch'>
-          <DisplayComponent time={time} />
-          <BtnStartCountUpComponent start={startCountUp} />
-        </div>
-      </div>
+    <ClockBackground>
+      <ClockContainer>
+        <CountUp>
+          <CountUpDisplay time={time} />
+          <BtnStartCountUp start={startCountUp} />
+        </CountUp>
+      </ClockContainer>
       <div className='count-downs'>
-        <BtnStartCountDownComponent time={time} text={'10초'} onClick={() => startCountDown} />
-        <BtnStartCountDownComponent time={time} text={'30초'} />
-        <BtnStartCountDownComponent time={time} text={'1분'} />
-        <BtnStartCountDownComponent time={time} text={'5분'} />
-        <BtnStartCountDownComponent time={time} text={'10분'} />
-        <BtnStartCountDownComponent time={time} text={'30분'} />
+        <CountDownTitle>
+          <span>카운트 다운</span>
+        </CountDownTitle>
+        <CountDownDisplay time={timeLeft} />
+        <BtnStartCountDown time={timeLeft} text={'10초'} start={startCountDown} />
+        <BtnStartCountDown time={timeLeft} text={'30초'} />
+        <BtnStartCountDown time={timeLeft} text={'1분'} />
+        <BtnStartCountDown time={timeLeft} text={'5분'} />
+        <BtnStartCountDown time={timeLeft} text={'10분'} />
+        <BtnStartCountDown time={timeLeft} text={'30분'} />
       </div>
-    </div>
+    </ClockBackground>
   </BasicContainer>);
 }
 
