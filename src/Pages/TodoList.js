e40f7c325d90d6916e1d777e77915ef96a417a7c
SHJcompany <93332615+SHJcompany@users.noreply.github.com>
2021-12-23 21:07:04 +0900

    to do list

--- src/Pages/TodoList.js
+++ src/Pages/TodoList.js
@@ -6,6 +6,7 @@ import TodoTemplate from '../Components/Test/TodoTemplate';
 import TodoHead from '../Components/Test/TodoHead';
 import TodoList from '../Components/Test/TodoList';
 import TodoCreate from '../Components/Test/TodoCreate';
+import { TodoProvider } from "../Components/Test/TodoContext";
 
 // const TodoList = () => {
 //   const me = useMe()
@@ -24,14 +25,14 @@ const GlobalStyle = createGlobalStyle`
 
 function Todo() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
