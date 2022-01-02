6c8017face8f14525f1921de8b50ca027b0eedd2
nlom0218 <nlom0218@naver.com>
2022-01-02 15:16:59 +0900

    AddStudentBtn Desktop

--- src/Components/List/AddStudentBox.js
+++ src/Components/List/AddStudentBox.js
@@ -1,37 +1,130 @@
+import { useReactiveVar } from '@apollo/client';
 import React, { useState } from 'react';
+import { BsPersonPlus } from 'react-icons/bs';
+import { FcUpload } from 'react-icons/fc';
 import styled from 'styled-components';
+import { darkModeVar } from '../../apollo';
 import useMedia from '../../Hooks/useMedia';
+import { color } from '../../styles';
 import CenterDndContainer from './Dorp/CenterDndContainer';
+
+// 연한 블루: #8dbdff
+// 진한 블루: #3b7dd8
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  grid-template-columns: 1fr;
+  position: relative;
 `
 
-const DndBox = styled.div`
-  min-height: 120px;
-  border: 3px dashed ${props => props.theme.popupBgColor};
-  border-radius: 10px;
-  border-radius: 0.625rem;
+const DndBox = styled.div` 
+  height: 380px;
+  max-height: 380px;
+  background-color: ${props => props.someDragging
+    ?
+    (props.darkMode ? "#8dbdff" : "#3b7dd8")
+    :
+    (props.darkMode ? "#3b7dd8" : "#8dbdff")};
+  transition: background-color 1s ease;
+`
+
+const UploadIcon = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
   display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  div {
-    opacity: 0.8;
-    font-size: 1.5em;
-    font-size: 1.5rem;
+  padding: 20px;
+  padding: 1.25rem;
+  border-radius: 50%;
+  border: 2px solid ${props => props.someDragging ?
+    (props.darkMode ? "#3b7dd8" : "#8dbdff")
+    :
+    (props.darkMode ? "#8dbdff" : "#3b7dd8")};
+  color: ${props => props.someDragging ?
+    (props.darkMode ? "#3b7dd8" : "#8dbdff")
+    :
+    (props.darkMode ? "#8dbdff" : "#3b7dd8")};
+  top: ${props => props.someDragging ? "35%" : "10%"};
+  font-size: 2.5em;
+  font-size: 2.5rem;
+  transition: border 1s ease, color 1s ease, top 1s ease;
+`
+
+const DragMsg = styled.div`
+  width: 100%;
+  position: absolute;
+  opacity: 0.8;
+  font-weight: 600;
+  top: 40%;
+  left: 50%;
+  transform: ${props => props.someDragging ? "translate(-70%, 0)" : "translate(-50%, 0)"};
+  opacity: ${props => props.someDragging ? "0" : "1"};
+  transition: transform 1s ease, opacity 1s ease;
+  display: grid;
+  justify-items: center;
+  row-gap: 20px;
+  row-gap: 1.25rem;
+  .mainMsg {
+    font-size: 1.75em;
+    font-size: 1.75rem;
+  }
+  .subMsg {
+    font-size: 0.875em;
+    font-size: 0.875rem;
   }
 `
 
+const AddBtnBox = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 70%;
+  left: 50%;
+  transform: ${props => props.someDragging ? "translate(-30%, 0)" : "translate(-50%, 0)"};
+  opacity: ${props => props.someDragging ? "0" : "1"};
+  transition: transform 1s ease, opacity 1s ease;
+  display: grid;
+  justify-items: center;
+  row-gap: 20px;
+  row-gap: 1.25rem;
+`
+
+const AddBtnMsg = styled.div``
+
+const AddBtn = styled.div`
+  cursor: pointer;
+  padding: 10px 20px;
+  padding: 0.625rem 1.25rem;
+  border-radius: 5px;
+  border-radius: 0.3125rem;
+  color: ${props => props.theme.bgColor};
+  background-color: ${props => props.someDragging ?
+    (props.darkMode ? "#3b7dd8" : "#8dbdff")
+    :
+    (props.darkMode ? "#8dbdff" : "#3b7dd8")};
+  transition: color 1s ease, background-color 1s ease;
+`
+
 const AddStudentBox = ({ listName, listId, setSuccessMsg, someDragging }) => {
+  const darkMode = useReactiveVar(darkModeVar)
+
   // 아무런 전혀 의미없는 값.. 단지 에러 방지
   const [__, set__] = useState(null)
 
   const media = useMedia()
+
+  const onClickAddBtn = () => { window.alert("학생 복수 추가 기능") }
   return (<Container>
-    {media === "Desktop" && <DndBox>
-      <div>학생을 Drag & Drop 하여 추가하기</div>
+    {media === "Desktop" && <DndBox someDragging={someDragging} darkMode={darkMode}>
+      <UploadIcon someDragging={someDragging} darkMode={darkMode}><BsPersonPlus /></UploadIcon>
+      <DragMsg someDragging={someDragging}>
+        <div className="mainMsg">Drag & Drop</div>
+        <div className="subMsg">오른쪽 학생 목록에서 학생을 드래그하여 명렬표에 추가하세요! 😁</div>
+      </DragMsg>
+      <AddBtnBox someDragging={someDragging}>
+        <AddBtnMsg>또는</AddBtnMsg>
+        <AddBtn someDragging={someDragging} darkMode={darkMode} onClick={onClickAddBtn}>학생 추가하기</AddBtn>
+      </AddBtnBox>
       <CenterDndContainer
         someDragging={someDragging}
         setSuccessMsg={setSuccessMsg}
