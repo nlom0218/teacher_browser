ad25a3a9ec8bc3fad666d4952a1c9f44ea89d396
nlom0218 <nlom0218@naver.com>
2022-01-16 14:08:24 +0900

    Trash AllRestore, AllDelete Btn

--- src/Pages/Trash.js
+++ src/Pages/Trash.js
@@ -5,6 +5,7 @@ import BasicContainer from '../Components/Shared/BasicContainer';
 import { SuccessMsg } from '../Components/Shared/styled/SuccessMsg';
 import StudentInTrash from '../Components/Trash/StudentInTrash';
 import { SEE_ALL_STUDENT_IN_TRASH_QUERY } from '../Graphql/Student/query';
+import { customMedia } from '../styles';
 
 const Container = styled.div`
   padding: 60px;
@@ -14,9 +15,43 @@ const Container = styled.div`
   row-gap: 2.5rem;
 `
 
+const TopLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 20px;
+  column-gap: 1.25rem;
+  row-gap: 20px;
+  row-gap: 1.25rem;
+  .trashBtn {
+    text-align: center;
+    color: ${props => props.theme.bgColor};
+    padding: 10px 20px;
+    padding: 0.625rem 1.25rem;
+    border-radius: 5px;
+    border-radius: 0.3125rem;
+    cursor: pointer;
+  }
+  ${customMedia.greaterThan("tablet")`
+    grid-template-columns: 1fr auto auto;
+    align-items: flex-end;
+  `}
+`
+
+const AllRestore = styled.div`
+  background-color: ${props => props.theme.btnBgColor};
+`
+
+const AllDelete = styled.div`
+  background-color: ${props => props.theme.redColor};
+`
+
 const Title = styled.div`
   font-size: 1.5em;
   font-size: 1.5rem;
+  grid-column: 1 / -1;
+  ${customMedia.greaterThan("tablet")`
+    grid-column: 1 / 2;
+  `}
 `
 
 const Student = styled.div`
@@ -49,7 +84,11 @@ const Trash = () => {
   }, [successMsg])
   return (<BasicContainer menuItem={true}>
     <Container>
-      <Title>휴지통</Title>
+      <TopLayout>
+        <Title>휴지통</Title>
+        <AllRestore className="trashBtn">전체 복구</AllRestore>
+        <AllDelete className="trashBtn">전체 삭제</AllDelete>
+      </TopLayout>
       <Student>
         {data?.seeAllStudent?.map((item, index) => {
           return <StudentInTrash key={index} item={item} setSuccessMsg={setSuccessMsg} />
