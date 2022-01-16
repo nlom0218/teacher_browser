ac167e70211136407e8f8312d80ee71d75cbcdd2
nlom0218 <nlom0218@naver.com>
2022-01-16 19:36:38 +0900

    LunchmenuOrigin hide not menu

--- src/Pages/Lunchmenu.js
+++ src/Pages/Lunchmenu.js
@@ -346,10 +346,10 @@ const Lunchmenu = () => {
             {me && <div onClick={() => onClickBtn("school")}>우리학교 식단표</div>}
           </LunchmenuBtn>
           <LunchmenuDetail>
-            <LunchmenuOrigin>
+            {menu && <LunchmenuOrigin>
               <div className="detail_title">✲ 원산지</div>
               <div>{origin.join(",")}</div>
-            </LunchmenuOrigin>
+            </LunchmenuOrigin>}
             <div>
               <div className="detail_title">✲ 알레르기정보</div>
               <div>요리명에 표시된 번호는 알레르기를 유발할수 있는 식재료입니다 (1.난류, 2.우유, 3.메밀, 4.땅콩, 5.대두, 6.밀, 7.고등어, 8.게, 9.새우, 10.돼지고기, 11.복숭아, 12.토마토, 13.아황산염, 14.호두, 15.닭고기, 16.쇠고기, 17.오징어, 18.조개류(굴,전복,홍합 등)</div>
