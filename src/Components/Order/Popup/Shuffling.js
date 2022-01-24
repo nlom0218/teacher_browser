dca7c4f39e4b1a794082be5d04430cb0f9a76e76
nlom0218 <nlom0218@naver.com>
2022-01-24 12:30:06 +0900

    RandomCircle

--- src/Components/Order/Popup/Shuffling.js
+++ src/Components/Order/Popup/Shuffling.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import BtnPopupContainer from '../../Shared/BtnPopupContainer';
+import RandomCircle from '../../Shared/RandomCircle';
 
 const StopBtn = styled.div`
 padding:20px 60px;
 padding : 1.25rem 3.75rem;
-background-color:${props=>props.theme.redColor};
+background-color:${props => props.theme.redColor};
 border-radius:5px;
 border-radius:0.3125rem;
-color : ${props=>props.theme.bgColor};
+color : ${props => props.theme.bgColor};
 cursor : pointer;
 `
 
-const Shuffling = ({onClickShuffleBtn}) => {
+const Shuffling = ({ onClickShuffleBtn }) => {
 
- 
 
-return (<BtnPopupContainer>
-<StopBtn onClick={()=> onClickShuffleBtn("finish")}>멈추기</StopBtn>
-</BtnPopupContainer>)}
-export default Shuffling ;
+
+  return (<BtnPopupContainer>
+    <RandomCircle />
+    <StopBtn onClick={() => onClickShuffleBtn("finish")}>멈추기</StopBtn>
+  </BtnPopupContainer>)
+}
+export default Shuffling;
 
 
 
---
+++ src/Components/Shared/RandomCircle.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import IcRandomCircleMint from '../../icons/RandomCircle/RandomCircleMint';
+import IcRandomCirclePink from '../../icons/RandomCircle/RandomCirclePink';
+import IcRandomCircleSky from '../../icons/RandomCircle/RandomCircleSky';
+import IcRandomCircleOrange from '../../icons/RandomCircle/RandomCircleOrange';
+import IcRandomCirclePurple from '../../icons/RandomCircle/RandomCirclePurple';
+import IcRandomCircleYellow from '../../icons/RandomCircle/RandomCircleYellow';
+
+const RandomCircle = () => {
+  return (<div>
+    <IcRandomCircleMint />
+    <IcRandomCirclePink />
+    <IcRandomCircleSky />
+    <IcRandomCircleOrange />
+    <IcRandomCirclePurple />
+    <IcRandomCircleYellow />
+  </div>);
+}
+
+export default RandomCircle;
