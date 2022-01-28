54c2f689c73f0256e15264d5a6cdbe2e794da773
nlom0218 <nlom0218@naver.com>
2022-01-28 23:23:38 +0900

    News, NameTable icon

---
+++ src/icons/NameTable/IcNameTable.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ReactComponent as NameTable } from "../../image/icons/NameTable.svg"
+
+const IcNameTable = () => {
+  return (<NameTable />
+  );
+}
+
+export default IcNameTable;
