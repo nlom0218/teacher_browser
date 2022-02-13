fd30dab0a421696556a00f76102a2ba43fd95b95
nlom0218 <nlom0218@naver.com>
2022-02-13 21:28:34 +0900

    Order Icon

---
+++ src/icons/Order/IcOrderClick.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ReactComponent as OrderClick } from "../../image/icons/OrderClick.svg"
+
+const IcOrderClick = () => {
+  return (<OrderClick />
+  );
+}
+
+export default IcOrderClick;
