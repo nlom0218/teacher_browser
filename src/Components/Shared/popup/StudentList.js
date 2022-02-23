1eacb2d59df24029e97668349e25d2b6afdb94bd
nlom0218 <nlom0218@naver.com>
2022-02-23 21:23:22 +0900

    StudentList NoDataMsg

--- src/Components/Shared/popup/StudentList.js
+++ src/Components/Shared/popup/StudentList.js
@@ -5,6 +5,9 @@ import { useQuery } from "@apollo/client";
 import StudentListItem from "./StudentListItem";
 import { SEE_ALL_STUDENT_LIST_QUERY } from "../../../Graphql/StudentList/query";
 import Loading from "../Loading";
+import routes from "../../../routes";
+import { useNavigate } from "react-router";
+import { inPopup } from "../../../apollo";
 
 const Container = styled.div`
   padding : 20px 0px;
@@ -18,11 +21,38 @@ const Container = styled.div`
   justify-items : center;
   text-align: center;
 `
+const NoDataMsg = styled.div`
+  grid-column: 1 / -1;
+  align-self: flex-start;
+  display: grid;
+  row-gap: 10px;
+  row-gap: 0.625rem;
+  justify-items: center;
+`
+
+const Msg = styled.div`
+  line-height: 160%;
+`
+
+const Btn = styled.div`
+  padding: 10px;
+  padding: 0.625rem;
+  border-radius: 5px;
+  border-radius: 0.3125rem;
+  background-color: ${props => props.theme.btnBgColor};
+  color: ${props => props.theme.bgColor};
+  cursor: pointer;
+`
 
 const StudentList = ({ setIsShuffle, page }) => {
+  const navigate = useNavigate()
+
   const { data, loading } = useQuery(SEE_ALL_STUDENT_LIST_QUERY)
 
-  console.log(data);
+  const onClickBtn = () => {
+    navigate(routes.list)
+    inPopup("createList")
+  }
 
   if (loading) {
     return <Loading page="popupPage" />
@@ -30,9 +60,15 @@ const StudentList = ({ setIsShuffle, page }) => {
 
   return (<PopupContainer>
     <Container>
-      {data?.seeStudentList.map((item, index) => {
-        return <StudentListItem key={index} item={item} setIsShuffle={setIsShuffle} page={page} />
-      })}
+      {data?.seeStudentList?.length === 0 ? <NoDataMsg>
+        <Msg>생성된 명렬표가 없습니다.</Msg>
+        <Msg>명렬표에서 생성해주세요.</Msg>
+        <Btn onClick={onClickBtn}>명렬표로 이동하기</Btn>
+      </NoDataMsg>
+        :
+        data?.seeStudentList.map((item, index) => {
+          return <StudentListItem key={index} item={item} setIsShuffle={setIsShuffle} page={page} />
+        })}
     </Container>
   </PopupContainer>
   );
