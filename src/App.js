e94cd8fa9f3d00380e53e77d70be34441a43d554
nlom0218 <nlom0218@naver.com>
2022-02-26 17:01:50 +0900

    new App.js

--- src/App.js
+++ src/App.js
@@ -77,7 +77,9 @@ function App() {
 
   useEffect(() => {
     disableBgThemeAni();
-    editBgTheme("nature")
+    if (!bgTheme) {
+      editBgTheme("nature")
+    }
   }, []);
 
   useEffect(() => {
