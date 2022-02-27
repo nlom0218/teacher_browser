6a55ab4a2976b61c85ce20e7e623160b840fe85c
soave424 <soave424@gmail.com>
2022-02-27 21:39:19 +0900

    시간표 음영

--- src/Components/Schedule/TableInItem.js
+++ src/Components/Schedule/TableInItem.js
@@ -25,7 +25,7 @@ const SubjectName = styled.div`
   white-space: nowrap;
   margin: 0px 10px;
   margin: 0px 0.625rem;
-  background-color: ${props => props.color};
+  background-color: ${(props) => props.color};
   ${customMedia.greaterThan("tablet")`
     font-size: ${(props) => props.fontSize}em;
     font-size: ${(props) => props.fontSize}rem;
@@ -34,52 +34,55 @@ const SubjectName = styled.div`
 `;
 
 const Span = styled.span`
+  background-color: ${(props) => props.color};
+  border-radius: 10px;
+  border-radius: 0.625rem;
 `;
 
-const Color = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  border-radius: 5px;
-  border-radius: 0.3125rem;
-  width: 100%;
-  height: ${props => props.fontSize / 2 * 16}px;
-  height: ${props => props.fontSize / 2}rem;
-  background-color: ${props => props.color};
-`
+// const Color = styled.div`
+//   position: absolute;
+//   bottom: 0;
+//   left: 0;
+//   border-radius: 5px;
+//   border-radius: 0.3125rem;
+//   width: 100%;
+//   height: ${(props) => (props.fontSize / 2) * 16}px;
+//   height: ${(props) => props.fontSize / 2}rem;
+//   background-color: ${(props) => props.color};
+// `;
 
 const HoverContainer = styled.div`
-position: absolute;
-font-size: 0.8rem;
-font-size: 0.8em;
-top: 0;
-left: 0;
-width: 100%;
-height: 100%;
-background-color: ${(props) => props.theme.cardHoverBg};
-border-radius: 5px;
-border-radius: 0.3125rem;
-animation: ${CardFadeIn} 0.6s ease forwards;
-color: ${(props) => props.theme.bgColor};
-display: grid;
-grid-template-columns: ${props => props.memo ? "auto 1fr" : "auto"};
-justify-items: ${props => !props.memo && "center"};
-align-items: center;
-padding: 0px 10px;
-padding: 0rem 0.625rem;
-column-gap: 5px;
-column-gap: 0.3125rem;
-svg {
-  display: flex;
-}
+  position: absolute;
+  font-size: 0.8rem;
+  font-size: 0.8em;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${(props) => props.theme.cardHoverBg};
+  border-radius: 5px;
+  border-radius: 0.3125rem;
+  animation: ${CardFadeIn} 0.6s ease forwards;
+  color: ${(props) => props.theme.bgColor};
+  display: grid;
+  grid-template-columns: ${(props) => (props.memo ? "auto 1fr" : "auto")};
+  justify-items: ${(props) => !props.memo && "center"};
+  align-items: center;
+  padding: 0px 10px;
+  padding: 0rem 0.625rem;
+  column-gap: 5px;
+  column-gap: 0.3125rem;
+  svg {
+    display: flex;
+  }
 `;
 
 const Memo = styled.div`
-overflow: hidden;
-text-overflow: ellipsis;
-white-space: nowrap;
-justify-self: flex-start;
-`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  justify-self: flex-start;
+`;
 
 const TableInItem = ({ num, subName, index, color, memo, fontSize }) => {
   const [hoverContainer, setHoverContainer] = useState(false);
@@ -103,8 +106,8 @@ const TableInItem = ({ num, subName, index, color, memo, fontSize }) => {
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
       >
-        <Span>{subName}</Span>
-        <Color color={color} fontSize={fontSize}></Color>
+        <Span color={color}>&nbsp;{subName}&nbsp;</Span>
+        {/* <Color color={color} fontSize={fontSize}></Color> */}
       </SubjectName>
       {hoverContainer ? (
         <HoverContainer memo={memo}>
