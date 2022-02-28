c143a55f5e16b25ca86872e0d5c3fc9aafe38780
nlom0218 <nlom0218@naver.com>
2022-02-28 23:58:44 +0900

    BottomContents

--- src/Components/Welcome/WelcomeSection.js
+++ src/Components/Welcome/WelcomeSection.js
@@ -7,6 +7,7 @@ import AlertMessage from '../Shared/AlertMessage';
 import { customMedia } from '../../styles';
 import WelcomeContents from './WelcomeContents';
 import TopContents from './TopContents';
+import BottomContents from './BottomContents';
 
 const MoveContainer = styled.div`
   position: absolute;
@@ -25,8 +26,8 @@ const Container = styled.div`
   padding: 2.5rem 1.25rem;
   display: grid;
   grid-template-rows: auto 1fr auto;
-  row-gap: 20px;
-  row-gap: 1.25rem;
+  row-gap: 40px;
+  row-gap: 2.5rem;
   align-items: flex-start;
   ${customMedia.greaterThan("tablet")`
     padding: 40px;
@@ -47,81 +48,6 @@ const MoveIcon = styled.div`
   }
 `
 
-const LogoContents = styled.div`
-  display: grid;
-  row-gap: 20px;
-  row-gap: 1.25rem;
-`
-
-const LogoMsg = styled.div`
-  display: grid;
-  column-gap: 20px;
-  column-gap: 1.25rem;
-  align-items: center;
-  row-gap: 20px;
-  row-gap: 1.25rem;
-  line-height: 120%;
-  ${customMedia.greaterThan("desktop")`
-    grid-template-columns: auto 1fr;
-  `}
-`
-
-const GoogleForm = styled.div`
-  justify-self: flex-end;
-  padding: 5px 20px;
-  padding: 0.3125rem 1.25rem;
-  background-color: ${props => props.theme.btnBgColor};
-  color: ${props => props.theme.bgColor};
-  transition: background-color 1s ease, color 1s ease;
-  border-radius: 5px;
-  border-radius: 0.3125rem;
-  cursor: pointer;
-  ${customMedia.greaterThan("desktop")`
-    justify-self: flex-start;
-  `}
-`
-
-const LogoList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  column-gap: 40px;
-  column-gap: 2.5rem;
-  transition: background-color 1s ease;
-  border-radius: 10px;
-  border-radius: 0.625rem;
-`
-
-const LogoItem = styled.div`
-  position: relative;
-`
-
-const LogoImg = styled.img`
-  width: 100%;
-`
-
-const LogoNum = styled.div`
-  position: absolute;
-  top: 5%;
-  left: 5%;
-  width: 20px;
-  width: 1.25rem;
-  height: 20px;
-  height: 1.25rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${props => props.theme.btnBgColor};
-  color: ${props => props.theme.bgColor};
-  transition: background-color 1s ease, color 1s ease;
-  border-radius: 50%;
-  ${customMedia.greaterThan("desktop")`
-    width: 40px;
-    width: 2.5rem;
-    height: 40px;
-    height: 2.5rem;
-  `}
-`
-
 const WelcomeSection = ({ welcomeSection, init, setInit, logoImageArr, me }) => {
   const [msg, setMsg] = useState(undefined)
 
@@ -137,20 +63,7 @@ const WelcomeSection = ({ welcomeSection, init, setInit, logoImageArr, me }) => 
       </MoveIcon>
       <TopContents me={me} />
       <WelcomeContents />
-      <LogoContents>
-        <LogoMsg>
-          <div>티처캔 캐릭터에 어울리는 멋진 이름을 지어주세요! 선정된 분들껜 소소한 선물이 기다리고 있습니다. 😁</div>
-          <GoogleForm onClick={() => { window.open("https://forms.gle/ih3oF6uPrn3Z1C3b7") }}>응모하러 가기</GoogleForm>
-        </LogoMsg>
-        <LogoList>
-          {logoImageArr.map((item, index) => {
-            return <LogoItem key={index}  >
-              <LogoImg src={item}></LogoImg>
-              <LogoNum>{index + 1}</LogoNum>
-            </LogoItem>
-          })}
-        </LogoList>
-      </LogoContents>
+      <BottomContents />
     </Container>
     <AlertMessage msg={msg} type="success" setMsg={setMsg} />
   </MoveContainer>);
