72c73ef4e7b96dc7bef8c1e561d7081d4e8044c1
nlom0218 <nlom0218@naver.com>
2022-03-03 23:00:30 +0900

    Theme ScreenTheme

--- src/Components/Shared/Theme.js
+++ src/Components/Shared/Theme.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { useReactiveVar } from '@apollo/client';
 import { darkModeVar, disableDarkMode, enableDarkMode } from '../../apollo';
 import { FaSun, FaMoon } from "react-icons/fa";
+import { BiExitFullscreen, BiFullscreen } from "react-icons/bi"
 
 const Wrapper = styled.div`
   position: fixed;
@@ -10,6 +11,32 @@ const Wrapper = styled.div`
   bottom: 1.25rem;
   right: 20px;
   right: 1.25rem;
+  display: grid;
+  grid-template-columns: auto auto;
+  z-index: 1;
+`
+
+const ScreenTheme = styled.div`
+  margin-right: 10px;
+  margin-right: 0.625rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  padding: 0.625rem;
+  border-radius: 50%;
+  box-shadow: 0px 2px 1px 0.5px rgba(0,0,0,0.2);
+  box-shadow: 0px 0.125rem 0.0625rem 0.03125rem rgba(0,0,0,0.2);
+  background: ${props => props.theme.bgColor};
+  transition: none;
+  cursor: pointer;
+  :hover {
+    background: ${props => props.theme.fontColor};
+    color: ${props => props.theme.bgColor};
+  }
+`
+
+const BackgroungTheme = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -23,7 +50,6 @@ const Wrapper = styled.div`
   transition: none;
   font-size: 0.75em;
   font-size: 0.75rem;
-  z-index: 1;
   cursor: pointer;
   :hover {
     background: ${props => props.theme.fontColor};
@@ -57,16 +83,21 @@ const Theme = () => {
     }
   }
   return (
-    <Wrapper onClick={onClickBtn}>
-      {darkMode ?
-        <LightModeBtn>
-          <FaSun /> 라이트 모드로 보기
+    <Wrapper>
+      <ScreenTheme>
+        <BiFullscreen />
+      </ScreenTheme>
+      <BackgroungTheme onClick={onClickBtn}>
+        {darkMode ?
+          <LightModeBtn>
+            <FaSun /> 라이트 모드로 보기
         </LightModeBtn>
-        :
-        <DarkModeBtn>
-          <FaMoon /> 다크 모드로 보기
+          :
+          <DarkModeBtn>
+            <FaMoon /> 다크 모드로 보기
         </DarkModeBtn>
-      }
+        }
+      </BackgroungTheme>
     </Wrapper>
   )
 }
