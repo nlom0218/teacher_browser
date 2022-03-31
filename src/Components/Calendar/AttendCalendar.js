7cef80211f13bb7e59a7e5a5ceaebaf572fb2476
nlom0218 <nlom0218@naver.com>
2022-03-31 23:17:26 +0900

    AttendInfoList, AttendInfoItem

--- src/Components/Calendar/AttendCalendar.js
+++ src/Components/Calendar/AttendCalendar.js
@@ -1,15 +1,62 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { compare } from '../../shared';
+
+const Container = styled.div`
+  padding: 5px;
+  row-gap: 0.3125rem;
+`
+
+const AttendInfoList = styled.div`
+  display: grid;
+  row-gap: 5px;
+  row-gap: 0.3125rem;
+`
+
+const AttendInfoItem = styled.div`
+  padding: 5px;
+  padding: 0.3125rem;
+  border-radius: 5px;
+  border-radius: 0.3125rem;
+  background-color: ${props => props.theme.cardBg};
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 10px;
+  column-gap: 0.625rem;
+  transition: background-color 1s ease;
+`
+
+const StudentName = styled.div`
+  /* font-weight: 600; */
+`
+
+const AttendType = styled.div`
+  /* font-weight: 600; */
+  color: ${props => props.attendType.includes("결석") ? props.theme.redColor : props.theme.btnBgColor};
+  transition: color 1s ease;
+`
 
 const AttendCalendar = ({ attendData, item }) => {
   const [attendInfo, setAttendInfo] = useState([])
-  console.log(attendInfo);
   useEffect(() => {
     if (attendData) {
-      const newAttendInfo = attendData?.seeAttendance?.filter(attend => attend.date === new window.Date(item.date).setHours(0, 0, 0, 0))
+      const newAttendInfo = attendData?.
+        seeAttendance?.
+        filter(attend => attend.date === new window.Date(item.date).setHours(0, 0, 0, 0))
+        .sort(compare("studentName"))
       setAttendInfo(newAttendInfo)
     }
   }, [attendData])
-  return (<div></div>);
+  return (<Container>
+    <AttendInfoList>
+      {attendInfo.map((item, index) => {
+        return <AttendInfoItem key={index}>
+          <StudentName>{item.studentName}</StudentName>
+          <AttendType attendType={item.type}>{item.type}</AttendType>
+        </AttendInfoItem>
+      })}
+    </AttendInfoList>
+  </Container>);
 }
 
 export default AttendCalendar;
