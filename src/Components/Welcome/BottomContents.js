1b0a632dd6081e5560e454b5c05e0db13b752251
nlom0218 <nlom0218@naver.com>
2022-04-10 23:53:01 +0900

    20220410 update

--- src/Components/Welcome/BottomContents.js
+++ src/Components/Welcome/BottomContents.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import { customMedia } from '../../styles';
+import React from "react";
+import styled from "styled-components";
+import { customMedia } from "../../styles";
 
 const Container = styled.div`
   display: grid;
@@ -9,7 +9,7 @@ const Container = styled.div`
   ${customMedia.greaterThan("desktop")`
     grid-template-columns: 1fr 1fr 1fr;
   `}
-`
+`;
 
 const LayoutOne = styled.div`
   background-color: rgba(241, 113, 113, 0.4);
@@ -18,7 +18,7 @@ const LayoutOne = styled.div`
   grid-template-rows: 1fr 1fr;
   padding: 20px;
   padding: 1.25rem;
-`
+`;
 
 const LayoutTwo = styled.div`
   background-color: rgb(139, 219, 41, 0.4);
@@ -27,7 +27,7 @@ const LayoutTwo = styled.div`
   grid-template-rows: 1fr 1fr;
   padding: 20px;
   padding: 1.25rem;
-`
+`;
 
 const Textarea = styled.div`
   align-self: center;
@@ -38,13 +38,13 @@ const Textarea = styled.div`
   row-gap: 10px;
   row-gap: 0.625rem;
   line-height: 160%;
-`
+`;
 
 const MainText = styled.div`
   font-size: 1.1em;
   font-size: 1.1rem;
   font-weight: 600;
-`
+`;
 
 const LayoutThree = styled.div`
   background-color: rgba(235, 151, 6, 0.4);
@@ -53,7 +53,7 @@ const LayoutThree = styled.div`
   grid-template-rows: 1fr 1fr;
   padding: 20px;
   padding: 1.25rem;
-`
+`;
 
 const Btn = styled.div`
   cursor: pointer;
@@ -61,53 +61,78 @@ const Btn = styled.div`
   justify-self: center;
   padding: 10px 20px;
   padding: 0.625rem 1.25rem;
-  border: 1px solid ${porps => porps.theme.fontColor};
+  border: 1px solid ${(porps) => porps.theme.fontColor};
   transition: border 1s ease;
   :hover {
-    background-color: ${props => props.theme.fontColor};
-    color: ${props => props.theme.bgColor};
+    background-color: ${(props) => props.theme.fontColor};
+    color: ${(props) => props.theme.bgColor};
     transition: background-color 0.4s ease, color 0.4s ease;
   }
-`
+`;
 
 const SubText = styled.div`
   opacity: 0.8;
   text-align: center;
   div {
-  :not(:last-child) {
-    margin-bottom: 10px;
-    margin-bottom: 0.625rem;
+    :not(:last-child) {
+      margin-bottom: 10px;
+      margin-bottom: 0.625rem;
     }
   }
-`
+`;
 
 const BottomContents = () => {
-  return (<Container>
-    <LayoutOne>
-      <Textarea>
-        <MainText>티처캔이 처음이신가요?</MainText>
-        <div>티처캔을 보다 잘 사용할 수 있는<br /> 방법을 알려드립니다. 😍</div>
-      </Textarea>
-      <Btn onClick={() => window.open("https://sparkly-corleggy-3e4.notion.site/718aaed6e5e54babb7efb97384bab836")}>설명서 바로가기</Btn>
-    </LayoutOne>
-    <LayoutTwo>
-      <Textarea>
-        <MainText>필요한 기능 및 오류가 있나요?</MainText>
-        <div>소중한 의견을 적극 반영하여<br />꾸준히 발전하겠습니다. 🤩</div>
-      </Textarea>
-      <Btn onClick={() => window.open("https://docs.google.com/forms/d/e/1FAIpQLSebZY_Z3Rtt573TmQ_TIwme7heipIfgGFjSZx5eKl-0bH9F5g/viewform")}>설문지 작성하기</Btn>
-    </LayoutTwo>
-    <LayoutThree>
-      <Textarea>
-        <MainText>About 티처캔</MainText>
-      </Textarea>
-      <SubText>
-        <div>티처캔 © 2022-2022</div>
-        <div>version 1.3.0</div>
-        <div>만든이 팀 초코</div>
-      </SubText>
-    </LayoutThree>
-  </Container>);
-}
+  return (
+    <Container>
+      <LayoutOne>
+        <Textarea>
+          <MainText>티처캔이 처음이신가요?</MainText>
+          <div>
+            티처캔을 보다 잘 사용할 수 있는
+            <br /> 방법을 알려드립니다. 😍
+          </div>
+        </Textarea>
+        <Btn
+          onClick={() =>
+            window.open(
+              "https://sparkly-corleggy-3e4.notion.site/718aaed6e5e54babb7efb97384bab836"
+            )
+          }
+        >
+          설명서 바로가기
+        </Btn>
+      </LayoutOne>
+      <LayoutTwo>
+        <Textarea>
+          <MainText>필요한 기능 및 오류가 있나요?</MainText>
+          <div>
+            소중한 의견을 적극 반영하여
+            <br />
+            꾸준히 발전하겠습니다. 🤩
+          </div>
+        </Textarea>
+        <Btn
+          onClick={() =>
+            window.open(
+              "https://docs.google.com/forms/d/e/1FAIpQLSebZY_Z3Rtt573TmQ_TIwme7heipIfgGFjSZx5eKl-0bH9F5g/viewform"
+            )
+          }
+        >
+          설문지 작성하기
+        </Btn>
+      </LayoutTwo>
+      <LayoutThree>
+        <Textarea>
+          <MainText>About 티처캔</MainText>
+        </Textarea>
+        <SubText>
+          <div>티처캔 © 2022-2022</div>
+          <div>version 1.4.0</div>
+          <div>만든이 팀 초코</div>
+        </SubText>
+      </LayoutThree>
+    </Container>
+  );
+};
 
 export default BottomContents;
