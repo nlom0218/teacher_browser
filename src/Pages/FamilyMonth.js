c1ab1e5a21cc129c5fb0c8df6f4686984aa424e3
nlom0218 <nlom0218@naver.com>
2022-04-19 19:55:07 +0900

    SlideBtn

--- src/Pages/FamilyMonth.js
+++ src/Pages/FamilyMonth.js
@@ -1,6 +1,10 @@
+import { useReactiveVar } from "@apollo/client";
 import React from "react";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
 import styled from "styled-components";
+import { darkModeVar } from "../apollo";
 import { HeaderHome } from "../Components/Shared/HeaderLink";
+import SlideBtn from "../Components/Shared/SlideBtn";
 
 const Container = styled.div`
   background: url("https://images.unsplash.com/photo-1612392987205-c53f0200a175?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
@@ -16,11 +20,29 @@ const Layout = styled.div`
   width: 100vw;
 `;
 
+const TopContents = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  justify-items: flex-end;
+`;
+
 const FamilyMonth = () => {
+  const darkMode = useReactiveVar(darkModeVar);
+  console.log(darkMode);
   return (
     <Container>
       <Layout>
-        <HeaderHome />
+        <TopContents>
+          <HeaderHome />
+          <SlideBtn
+            leftContents={true}
+            rigthContents={false}
+            state={darkMode}
+            darkMode={true}
+            leftIcon={<BsMoonFill style={{ color: "yellow" }} />}
+            rigthIcon={<BsSunFill style={{ color: "tomato" }} />}
+          />
+        </TopContents>
       </Layout>
     </Container>
   );
