3c2bdf3368fcee9d9eee66efa9dd9c819ff1c6e5
nlom0218 <nlom0218@naver.com>
2022-04-21 19:44:52 +0900

    YouTubeItem

--- src/Components/FamilyMonth/MainYouTube.js
+++ src/Components/FamilyMonth/MainYouTube.js
@@ -3,75 +3,72 @@ import styled from "styled-components";
 import YouTubeItem from "./YouTubeItem";
 
 const YouTubeList = styled.div`
+  padding: 20px;
+  padding: 1.25rem;
   align-self: flex-start;
   display: grid;
-  grid-template-columns: 1fr;
-  row-gap: 1px;
-  border: 1px solid ${props=>props.theme.cardBorder};
-  background-color: ${props=>props.theme.cardBorder};
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  row-gap: 40px;
+  row-gap: 2.5rem;
+  column-gap: 40px;
+  column-gap: 2.5rem;
 `;
 
 const youtubeList = [
   {
     url: "https://www.youtube.com/watch?v=cS-IiArGmcU",
     title: "김진호-가족사진",
+    bgColor: "#F38181",
   },
   {
     url: "https://www.youtube.com/watch?v=7c64qLxZUb4&t=536s",
     title: "김진호-가족사진 속에 숨겨둔 어머니의 10000원",
+    bgColor: "#FCE38A",
   },
   {
     url: "https://www.youtube.com/watch?v=wFeFIN8CI0A",
     title: "김진호-가족사진",
+    bgColor: "#EAFFD0",
   },
   {
     url: "https://www.youtube.com/watch?v=G5kzUpWAusI&t=7s",
     title: "김진호-가족사진",
+    bgColor: "#95E1D3",
   },
   {
     url: "https://www.youtube.com/watch?v=IE8HIsIrq4o&t=37s",
     title: "김진호-가족사진",
+    bgColor: "#A8E6CF",
   },
   {
     url: "https://www.youtube.com/watch?v=1z4NjPc4_i0",
     title: "김진호-가족사진",
+    bgColor: "#AA96DA",
   },
   {
     url: "https://www.youtube.com/watch?v=P0ikE6tFbas",
     title: "김진호-가족사진",
+    bgColor: "#FCE38A",
   },
   {
     url: "https://www.youtube.com/watch?v=G5kzUpWAusI&t=7s",
     title: "김진호-가족사진",
+    bgColor: "#FCBAD3",
   },
   {
     url: "https://www.youtube.com/watch?v=IE8HIsIrq4o&t=37s",
     title: "김진호-가족사진",
+    bgColor: "#A8D8EA",
   },
   {
     url: "https://www.youtube.com/watch?v=1z4NjPc4_i0",
     title: "김진호-가족사진",
+    bgColor: "#FFFFD2",
   },
   {
     url: "https://www.youtube.com/watch?v=P0ikE6tFbas",
     title: "김진호-가족사진",
-  },
-  ,
-  {
-    url: "https://www.youtube.com/watch?v=G5kzUpWAusI&t=7s",
-    title: "김진호-가족사진",
-  },
-  {
-    url: "https://www.youtube.com/watch?v=IE8HIsIrq4o&t=37s",
-    title: "김진호-가족사진",
-  },
-  {
-    url: "https://www.youtube.com/watch?v=1z4NjPc4_i0",
-    title: "김진호-가족사진",
-  },
-  {
-    url: "https://www.youtube.com/watch?v=P0ikE6tFbas",
-    title: "김진호-가족사진",
+    bgColor: "#DBE2EF",
   },
 ];
 
--- src/Components/FamilyMonth/YouTubeItem.js
+++ src/Components/FamilyMonth/YouTubeItem.js
@@ -3,37 +3,50 @@ import styled from "styled-components";
 import getYouTubeId from "get-youtube-id";
 
 const SYouTubeItem = styled.div`
-  background-color: ${props=>props.theme.originBgColor};
+  background-color: ${(props) => props.bgColor};
   display: grid;
-  grid-template-columns: 1fr auto;
+  grid-template-rows: 1fr 2fr;
   cursor: pointer;
   position: relative;
+  border-radius: 20px;
+  border-radius: 1.25rem;
+  box-shadow: ${(props) =>
+    props.hover
+      ? "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px"
+      : "rgba(0, 0, 0, 0.24) 0px 3px 8px"};
+  transform: ${(props) => (props.hover ? "scale(1.02)" : "scale(1)")};
+  transition: transform 0.4s ease, box-shadow 0.4s ease;
   :hover {
-    background-color: ${props=>props.theme.hoverColor};
+    /* background-color: ${(props) => props.theme.hoverColor}; */
   }
 `;
 
 const YouTubeImg = styled.img`
- object-fit: cover;
-  width: 120px;
-  height: 60px;
+  padding: 20px;
+  padding: 1.25rem;
+  object-fit: cover;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
 `;
 
 const YouTubeTitle = styled.div`
+  color: rgb(10, 10, 10);
   padding: 10px 20px;
   padding: 0.625rem 1.5rem;
   display: grid;
-  align-items: center;
+  align-items: flex-start;
   line-height: 120%;
 `;
 
-const YouTubeItem = ({ url, title }) => {
+const YouTubeItem = ({ url, title, bgColor }) => {
   const [hover, setHover] = useState(false);
   const getYouTubeImg = (youTubeUrl) => {
     return `https://img.youtube.com/vi/${getYouTubeId(youTubeUrl)}/0.jpg`;
   };
   return (
     <SYouTubeItem
+      bgColor={bgColor}
       hover={hover}
       onMouseEnter={() => {
         setHover(true);
@@ -42,9 +55,11 @@ const YouTubeItem = ({ url, title }) => {
         setHover(false);
       }}
     >
-      <YouTubeTitle>{title}
-      </YouTubeTitle>
-      <YouTubeImg src={getYouTubeImg(url)} youtubeImg={getYouTubeImg(url)}></YouTubeImg>
+      <YouTubeImg
+        src={getYouTubeImg(url)}
+        youtubeImg={getYouTubeImg(url)}
+      ></YouTubeImg>
+      <YouTubeTitle>{title}</YouTubeTitle>
     </SYouTubeItem>
   );
 };
