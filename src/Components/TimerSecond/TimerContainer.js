cd809dd2cd3dd74d98c80061fcc86a3ee5d591ee
nlom0218 <nlom0218@naver.com>
2022-06-06 16:18:45 +0900

    TimerContainer MemoBox

--- src/Components/TimerSecond/TimerContainer.js
+++ src/Components/TimerSecond/TimerContainer.js
@@ -1,13 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 import { fullScreenMode, smallScreenMode } from "../../apollo";
-import { color } from "../../styles";
+import { color, customMedia } from "../../styles";
 
 const Container = styled.div`
   min-height: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+
+const MemoBox = styled.div`
+  font-size: 4em;
+  font-size: 4rem;
+  padding: 20px 40px;
+  padding: 1.25rem 2.5rem;
+  line-height: 120%;
+  font-weight: 600;
+  text-align: center;
+  ${customMedia.greaterThan("desktop")`
+    font-size: 6em;
+    font-size: 6rem;
+  `}
+  background-color: ${(props) => props.theme.bgColor};
+  border-radius: 20px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 `;
 
 const TimeBox = styled.div`
@@ -54,6 +72,7 @@ const TimerContainer = ({
   };
   return (
     <Container>
+      <MemoBox>4교시 국어 수업 준비, 준비물은 없음</MemoBox>
       <TimeBox
         isPopup={isPopup}
         onClick={onClickTiemBox}
