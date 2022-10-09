3f6aa508170876ae9ee6b8ea3c68b91d1aaad285
nlom0218 <nlom0218@naver.com>
2022-10-09 17:35:03 +0900

    Qrcode popup url #2, Login BackBtn

--- src/Pages/Qrcode.js
+++ src/Pages/Qrcode.js
@@ -4,15 +4,18 @@ import BasicContainer from "../Components/Shared/BasicContainer";
 import Qrcodemake from "../Components/Qrcode/Qrmake"; //생성화면
 
 import useMe from "../Hooks/useMe";
+import { useParams } from "react-router-dom";
 
 const QrcodeMake = () => {
   const [mode, setMode] = useState("make");
   const [url, setUrl] = useState(undefined); //생성화면 입력 혹은 보관함에서 클릭한 url주소
 
+  const { popup } = useParams();
+
   const me = useMe();
 
   return (
-    <BasicContainer menuItem={true}>
+    <BasicContainer menuItem={true} isWindowPopup={Boolean(popup)}>
       <QrcodeUrlContext.Provider value={{ me, mode, setMode, url, setUrl }}>
         <Qrcodemake me={me} />
       </QrcodeUrlContext.Provider>
