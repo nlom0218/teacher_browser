ea07fc6bae7bc3ff8e8a853e8a351b83f21bb637
nlom0218 <nlom0218@naver.com>
2022-10-30 22:07:21 +0900

    feat: 세부 명렬표 페이지에서 대표 명렬표로 설정할 수 있는 기능 추가하기

---
+++ src/Components/List/SetRepresentList.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import styled from "styled-components";
+
+interface IIconProps {
+  isRepresent: boolean;
+}
+
+const Container = styled.div``;
+
+const Layout = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Icon = styled.div<IIconProps>`
+  margin-right: 10px;
+  margin-right: 0.625rem;
+  color: ${(props) => props.isRepresent && props.theme.redColor};
+  cursor: pointer;
+  svg {
+    font-size: 1.5em;
+    font-size: 1.5rem;
+  }
+`;
+
+const Message = styled.div`
+  font-size: 1.25em;
+  font-size: 1.25rem;
+  font-weight: 600;
+`;
+
+const SetRepresentList = () => {
+  const [isRepresent, setIsRepresent] = useState(true);
+
+  const onClickIcon = () => {
+    setIsRepresent((prev) => !prev);
+  };
+  return (
+    <Container>
+      {isRepresent ? (
+        <Layout>
+          <Icon isRepresent={isRepresent} onClick={onClickIcon}>
+            <AiFillStar />
+          </Icon>
+          <Message>대표 명렬표</Message>
+        </Layout>
+      ) : (
+        <Layout>
+          <Icon isRepresent={isRepresent} onClick={onClickIcon}>
+            <AiOutlineStar />
+          </Icon>
+        </Layout>
+      )}
+    </Container>
+  );
+};
+
+export default SetRepresentList;
