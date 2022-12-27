37936c9e56fa737602f7308835ada35816f4dc3d
nlom0218 <nlom0218@naver.com>
2022-12-27 23:07:47 +0900

    feat: 디데이 계산하기

--- src/Components/SeoWonju/Contents.tsx
+++ src/Components/SeoWonju/Contents.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -15,7 +16,9 @@ const Container = styled.div`
   border-radius: 0.625rem;
 `;
 
-const Box = styled.div`
+const Box = styled.div``;
+
+const Text = styled.div`
   font-family: "Song Myung";
   font-size: 4em;
   font-size: 4rem;
@@ -23,12 +26,33 @@ const Box = styled.div`
   line-height: 120%;
 `;
 
+const CountDown = styled.div``;
+
 const Contents = () => {
+  const meetDay = Number(new Date("2032-12-30"));
+  const [today, setToday] = useState(Number(new Date()));
+
+  const diff = meetDay - today;
+
+  const diffDay = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const diffHour = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const diffMin = Math.floor((diff / (1000 * 60)) % 60);
+  const diffSec = Math.floor((diff / 1000) % 60);
+
+  useEffect(() => {
+    const countDown = setInterval(() => {
+      setToday(Number(new Date()));
+    }, 1000);
+    return () => clearInterval(countDown);
+  }, []);
   return (
     <Container>
       <Box>
-        <div className="emphasis">2022학년도 서원주 6학년 2반 </div>
-        <div>우리가 다시 만날 그 날</div>
+        <Text>
+          <div className="emphasis">2022학년도 서원주 6학년 2반 </div>
+          <div>우리가 다시 만날 그 날</div>
+        </Text>
+        <CountDown>{`${diffDay}일 ${diffHour}시간 ${diffMin}분 ${diffSec}초`}</CountDown>
       </Box>
     </Container>
   );
