5a8c4a434545fb92a2dc139a738f541864277f9d
nlom0218 <nlom0218@naver.com>
2023-02-02 16:34:02 +0900

    feat: 역할 수정으로 돌아가기

--- src/Pages/RolesAddStudents.tsx
+++ src/Pages/RolesAddStudents.tsx
@@ -1,24 +1,34 @@
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import BtnContainer from "../Components/Roles/Register/BtnContainer";
 import Form from "../Components/Roles/Register/Form";
 import Title from "../Components/Roles/Register/Title";
 import RolesGraph from "../Components/Roles/RolesGraph";
+import routes from "../routes";
 
 interface IProps {
   setErrMsg: React.Dispatch<React.SetStateAction<null | string>>;
 }
 
 const RolesAddStudents = ({ setErrMsg }: IProps) => {
+  const navigate = useNavigate();
   const { roles } = JSON.parse(localStorage.getItem("roleDetails") || "{}");
 
   const { register } = useForm({
     mode: "onChange",
   });
+
+  const onClickEditBtn = () => {
+    navigate(`${routes.roles}/setting`);
+  };
+
   return (
     <Form>
       <Title>1인 1역 - 학생 입력하기</Title>
       <BtnContainer isAddStudent={true}>
-        <div className="line-btn btn">역할 수정</div>
+        <div className="line-btn btn" onClick={onClickEditBtn}>
+          역할 수정
+        </div>
         <input type="submit" value="저장" className="save-btn btn" />
       </BtnContainer>
       <RolesGraph register={register} isAddStudent={true} roles={roles} />
