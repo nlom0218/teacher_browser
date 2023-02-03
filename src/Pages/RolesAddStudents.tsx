d9044022760a89013d1e1371ff76587f37ed4836
nlom0218 <nlom0218@naver.com>
2023-02-03 23:00:25 +0900

    feat: 1인 1역 저장하기

--- src/Pages/RolesAddStudents.tsx
+++ src/Pages/RolesAddStudents.tsx
@@ -24,6 +24,15 @@ const RolesAddStudents = ({ setErrMsg, setMsg }: IProps) => {
     navigate(`${routes.roles}/setting`);
   };
 
+  const onClickSaveBtn = () => {
+    // 어디까지 허용할것인가?
+    // 역할이 비어있는 경우 허용?
+    // 역할 부여가 안된 학생이 있는 경우 허용?
+    // 유효성 검사 이후, DB에 로컬스토리지 내용 저장
+    // DB에 저장이후 로컬스토리지 삭제
+    // 메인 페이지로 이동 후 DB에 있는 정보 가져오기(현재 날짜에 따른 1인 1역)
+  };
+
   return (
     <Form>
       <Title>1인 1역 - 학생 입력하기</Title>
@@ -32,7 +41,7 @@ const RolesAddStudents = ({ setErrMsg, setMsg }: IProps) => {
           역할 수정
         </div>
         <div>{`${format(new Date(startDate), "yy.MM.dd")} ~ ${format(new Date(endDate), "yy.MM.dd")}`}</div>
-        <input type="submit" value="저장" className="save-btn btn" />
+        <input type="submit" value="저장" className="save-btn btn" onClick={onClickSaveBtn} />
       </BtnContainer>
       <RolesGraph register={register} isAddStudent={true} roles={roles} setMsg={setMsg} />
     </Form>
