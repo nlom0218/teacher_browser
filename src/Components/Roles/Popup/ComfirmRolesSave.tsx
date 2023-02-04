36c7f3779ceebdc447ec3c44fb9f172273e2845e
nlom0218 <nlom0218@naver.com>
2023-02-04 15:11:36 +0900

    feat: 역할, 학생 유효성 검사 실패 시 팝업 띄우기

--- src/Components/Roles/Popup/ComfirmRolesSave.tsx
+++ src/Components/Roles/Popup/ComfirmRolesSave.tsx
@@ -1,5 +1,38 @@
+import styled from "styled-components";
 import { outPopup } from "../../../apollo";
 import BtnPopupContainer from "../../Shared/BtnPopupContainer";
+
+const Layout = styled.div`
+  display: grid;
+  row-gap: 20px;
+  row-gap: 1.25rem;
+  line-height: 140%;
+  color: ${(props) => props.theme.bgColor};
+  .comfirm-msg {
+    color: ${(props) => props.theme.redColor};
+    font-weight: 700;
+  }
+`;
+
+const Text = styled.div`
+  text-align: center;
+`;
+
+const BtnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 20px;
+  column-gap: 1.25rem;
+  div {
+    background-color: ${(props) => props.theme.btnBgColor};
+    padding: 10px;
+    padding: 0.625rem;
+    text-align: center;
+    border-radius: 5px;
+    border-radius: 0.3125rem;
+    cursor: pointer;
+  }
+`;
 
 interface IProps {
   studentVaild: boolean;
@@ -12,7 +45,16 @@ const ComfirmRolesSave = ({ studentVaild, rolesVaild }: IProps) => {
   };
   return (
     <BtnPopupContainer preventOutPoup={true}>
-      <div onClick={onClickCancelBtn}>ddd</div>
+      <Layout>
+        {!studentVaild && <Text className="comfirm-msg">역할이 등록되지 않은 학생이 존재합니다.</Text>}
+        {!rolesVaild && <Text className="comfirm-msg">학생이 등록되지 않은 역할이 존재합니다.</Text>}
+        <Text>이대로 저장을 진행하려면 저장하기 버튼을 클릭</Text>
+        <Text>추가 등록을 하려면 돌아가기를 클릭</Text>
+        <BtnLayout>
+          <div>저장하기</div>
+          <div onClick={onClickCancelBtn}>돌아가기</div>
+        </BtnLayout>
+      </Layout>
     </BtnPopupContainer>
   );
 };
